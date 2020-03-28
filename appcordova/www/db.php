be50391efa0f0fd6fbb4b1259e228f3148be0740
iqbalzakasih <zakasihzakasih@gmail.com>
2020-03-28 18:45:58 +0700

    nama_commit

---
+++ appcordova/www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id12789063_zakasih","Panser1932","id12789063_akademik") or die ("could not connect database");
+?>
